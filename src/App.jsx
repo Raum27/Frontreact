c931d1810e9ee7c8f728b5402ab23958b2b7bd5c
Raum27 <aumratchai123@gmail.com>
2024-01-02 15:05:44 +0700

    reamin Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios'
-
+import { Link } from "react-router-dom"
+import hero from 'C:/Users/Raum/Desktop/FRONT/src/assets/he1.jpg'
+import momo from './assets/ezgif.com-resize.gif'
 
 function App() {
-    const ip = 'http://127.0.0.1:5000'
-    
-    useEffect(() => {
-        axios.get(ip).then((res) => {
-            console.log(res.data)
-        }).catch((error) => {
-            console.log(error)
-        })
-    }, [])
-
     return (
         <>
-            <h1>Hello wordl!!!</h1>
+            <div className="navbar bg-base-100 border-b-2 flex justify-between">
+                <h1 className="btn  btn-ghost text-rose-500 text-xl">Automated Face Selection and Censoring </h1>
+                <div >
+                    <Link to="/UPLOADIMAGE"><button className="btn btn-error btn-lg mx-3 text-zinc-100">TRY IMAGES</button></Link>
+                    <Link to="/UPLOADVIDEO"><button className="btn btn-error btn-lg mx-2 text-zinc-100" >TRY VIDEO</button></Link>
+                </div>
+            </div>
+            <div className="hero min-h-dvh bg-base-200">
+                <div className="hero-content flex-col lg:flex-row">
+                    <img src={hero} className="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 className="text-5xl font-bold">Images!</h1>
+                        <p className="py-6">information is important in this age. The world has changed, and many people emphasize privacy even Images. This website can help you to conceal Other Person's Information in your images.</p>
+                        <Link to="/UPLOADIMAGE">  <button className="btn btn-primary">Start image</button></Link>
+                    </div>
+                </div>
+            </div>
+
+            <div className="hero min-h-lvh" style={{ backgroundImage: `url(${momo})` }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">Video :)</h1>
+                        <p className="mb-5">don't waste your time. Try your video with this Automated Face Selection and Censoring</p>
+                        <Link to="/UPLOADVIDEO"><button className="btn btn-primary">Start video</button></Link>
+                    </div>
+                </div>
+            </div>
+
+
         </>
-    );
+    )
 }
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
-
+import hero from '../assets/he1.jpg'
+import momo from '../assets/ezgif.com-resize.gif'
 
 
 function Home() {
@@ -12,7 +13,29 @@ function Home() {
           <Link to="/UPLOADVIDEO"><button className="btn btn-error btn-lg mx-2 text-zinc-100" >TRY VIDEO</button></Link>
         </div>
       </div>
-      <img src="https://plus.unsplash.com/premium_photo-1674718013659-6930c469e641?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="NOne" />
+      <div className="hero min-h-dvh bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <img src={hero} className="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="text-5xl font-bold">Images!</h1>
+            <p className="py-6">information is important in this age. The world has changed, and many people emphasize privacy even Images. This website can help you to conceal Other Person's Information in your images.</p>
+            <Link to="/UPLOADIMAGE">  <button className="btn btn-error">Start image</button></Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="hero min-h-lvh" style={{ backgroundImage: `url(${momo})` }}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Video :)</h1>
+            <p className="mb-5">don't waste your time. Try your video with this Automated Face Selection and Censoring</p>
+            <Link to="/UPLOADVIDEO"><button className="btn btn-error">Start video</button></Link>
+          </div>
+        </div>
+      </div>
+
+
     </>
   )
 }
